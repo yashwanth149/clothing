88c1a208fca2473e2a24e53ca2601171d11a3303
yashwanthN149 <yashwanthN149@gmail.com>
2022-07-30 11:51:29 +0530

    update page

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ const buttn = document.querySelector(".load");
 buttn.addEventListener("click", function () {
   alert("Sorry still creating shoping list..");
 });
+
+const menuBtn = document.querySelector('.burgur');
+const fullBar = document.querySelector('.respons-nav');
+
+menuBtn.addEventListener('click', () =>  {
+  menuBtn.classList.toggle('is-active');
+  fullBar.style.left = 0;
+});
+
+
